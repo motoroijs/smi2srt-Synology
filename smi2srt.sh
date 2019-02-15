4dc4398c5e6f461ff4bddc1da36e24363a5e66be
YG Kang <yklovett@naver.com>
2019-02-15 09:23:02 +0900

    Update smi2srt.sh
    smi 자막 파일에 <body> 가 포함되어있지 않으면, 기존 파일은 변환을 실패하고 0byte 의 ko.srt 파일로 변환하는 문제 확인
    그래서 <body> 가 있을 경우 기존 방식으로 변환
    <body> 가 없을경우 axfree님의 smi2srt로 변환하는 방식으로 변경
     (https://www.npmjs.com/package/smi2srt)  를 참고하여 패키지 설치 필요

--- smi2srt.sh
+++ smi2srt.sh
@@ -1,19 +1,58 @@
 #!/bin/sh
 
-#convert smi to ko.srt
-targetdir='/volume1/video/해외영화'
-targetdir2='/volume1/video/해외시리즈'
-smidir='/volume1/homes/계정명/script/smi2srt'
+# convert smi to ko.srt
+# if <body> include : smi2srt by "George Shuklin"
+# if <body> doesn`t include : smi2srt by axfree npm
+# sudo npm install smi2srt -g
 
-find $targetdir $targetdir2 -name "*.smi" | grep -v @eaDir | while read oldfile
-	do
-	  newfile=${oldfile%smi}ko.srt
-		if [ -f "$newfile" ]
+targetdir='/volume1/video/해외영화'
+targetdir2='/volume1/video/4K UHD (2160p)'
+smidir='/volume1/homes/user/script/smi2srt'
+
+find "$targetdir" "$targetdir2" -name "*.smi" | grep -v "@eaDir" | grep -v "#recycle" | while read filename
+    do
+		# sort <body> contain
+		grep -i "<body>" "$filename"
+		if [ $? -eq 0 ]
 			then
-		        	echo "$oldfile" "Skip!!"
-		else
-        		echo "$oldfile => $newfile" converted
-			$smidir/smi2srt "$oldfile" "$newfile"
-			rm -f "$oldfile"
+				# include <body>
+				echo "$filename" >> $smidir/grep_list
+				# doesn`t include <body>
+			else
+				echo "$filename" >> $smidir/grep_notlist
 		fi
 	done
+
+		# convert include <body> in subtitles
+	while read oldfile
+		do
+			newfile=${oldfile%smi}ko.srt
+			    if [ -f "$newfile" ]
+			        then
+				        echo "$oldfile" "Skip!!"
+				    else
+				        $smidir/smi2srt "$oldfile" "$newfile"
+				        echo "$oldfile => $newfile" converted
+						# remove origin file
+				        rm -f "$oldfile"
+			    fi
+		done < $smidir/grep_list
+
+       # convert doesn`t include <body> in subtitles
+	while read oldfile2
+		do
+			newfile2=${oldfile2%smi}ko.srt
+				if [ -f "$newfile2" ]
+					then
+						echo "$oldfile2" "Skip!!"
+					else
+						/usr/local/bin/smi2srt -n "$oldfile2"
+						echo "$oldfile2 => $newfile2" converted
+						# remove origin file
+						rm -f "$oldfile2"
+				fi
+		done < $smidir/grep_notlist
+
+sleep 1
+# remove smi file list
+rm -f $smidir/grep_*list
