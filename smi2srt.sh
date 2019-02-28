44594eace706d10d481d68c7f449c2296418d36c
YG Kang <yklovett@naver.com>
2019-02-28 16:23:16 +0900

    Update smi2srt.sh
    변환전 원본 자막을 특정경로에 백업하도록 변경

--- smi2srt.sh
+++ smi2srt.sh
@@ -6,17 +6,18 @@
 targetdir='/volume1/video/해외영화'
 targetdir2='/volume1/video/4K UHD (2160p)'
 smidir='/volume1/homes/yklovett/script/smi2srt'
+bkupdir='/volume1/log/smi2srt'
+today=`date '+%y%m%d'`
 
 find "$targetdir" "$targetdir2" -name "*.smi" | grep -v "@eaDir" | grep -v "#recycle" | while read filename
     do
-
 		# sort <body> contain
 		grep -i "<body>" "$filename"
 		if [ $? -eq 0 ]
 			then
 				# include <body>
 				echo "$filename" >> $smidir/grep_list
-				# doesn`t include <body>
+			# doesn`t include <body>
 			else
 				echo "$filename" >> $smidir/grep_notlist
 		fi
@@ -28,15 +29,17 @@ then
 	while read oldfile
 	do
 		newfile=${oldfile%smi}ko.srt
-		    if [ -f "$newfile" ]
-	        then
-		        echo "$oldfile" "Skip!!"
-		    else
-		        $smidir/smi2srt "$oldfile" "$newfile"
-		        echo "$oldfile => $newfile" converted
-				# remove origin file
-		        rm -f "$oldfile"
-		    fi
+	    if [ -f "$newfile" ]
+        then
+	        echo "$oldfile" "Skip!!"
+	    else
+	        $smidir/smi2srt "$oldfile" "$newfile"
+	        echo "$oldfile => $newfile" converted
+	        # rm -f "$oldfile"
+			# backup origin smi
+			mkdir -p $bkupdir/$today
+			mv "$oldfile" $bkupdir/$today
+	    fi
 	done < $smidir/grep_list
 sleep 1
 rm -f $smidir/grep_list
@@ -44,21 +47,23 @@ rm -f $smidir/grep_list
 else
 	echo "### no subtitles to convert (include body)"
 fi
+
 # convert doesn`t include <body> in subtitles
 if [ -f "$smidir/grep_notlist" ]
 then
 	while read oldfile2
 	do
 		newfile2=${oldfile2%smi}ko.srt
-				if [ -f "$newfile2" ]
-				then
-					echo "$oldfile2" "Skip!!"
-				else
-					/usr/local/bin/smi2srt -n "$oldfile2"
-					echo "$oldfile2 => $newfile2" converted
-					# remove origin file
-					rm -f "$oldfile2"
-				fi
+		if [ -f "$newfile2" ]
+			then
+			echo "$oldfile2" "Skip!!"
+		else
+			/usr/local/bin/smi2srt -n "$oldfile2"
+			echo "$oldfile2 => $newfile2" converted
+			# rm -f "$oldfile2"
+			# backup origin smi
+			mv "$oldfile2" $bkupdir/$today
+		fi
 	done < $smidir/grep_notlist
 sleep 1
 rm -f $smidir/grep_notlist
