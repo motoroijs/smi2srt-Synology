f254eeeb252346d2a4eebe51427e50b4b32933e7
YG Kang <yklovett@naver.com>
2019-04-09 10:07:55 +0900

    Add files via upload
    기존 존재하던 ko.srt 자막파일들의 charset 을 UTF-8 로 변환하는 스크립트

---
+++ charconvert.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+export LC_ALL=en_US.utf8
+export LANG=en_US.utf8
+targetdir='/volume1/download/series'
+
+find "$targetdir" -type f -name "*.ko.srt" | grep -v "@eaDir" | grep -v "#recycle" | while read oldfile
+	do
+		newfile=${oldfile}bk
+		rm -f "$newfile"
+		charset=`/bin/chardetect "$oldfile" | awk -F":" '{print $2}' | awk -F" " '{print $1}'`
+		echo "$charset" | grep -i "no"
+			if [ $? -eq 0 ]
+				then
+					echo "################"
+					echo "$oldfile"
+					echo "charset analysis fail. skip convert"
+				else
+					echo "$charset" | grep -i "utf"
+						if [ $? -eq 0 ]
+			                then
+								echo "################"
+								echo "$oldfile"
+				                echo "charset is UTF-8. skip convert"
+		                else
+								echo "#################"
+								echo "$oldfile"
+								echo "converting $charset to UTF-8"
+								mv "$oldfile" "$newfile"
+								piconv -c -f "$charset" -t utf8 "$newfile" > "$oldfile"
+								rm -f "$newfile"
+						fi
+			fi
+	done
